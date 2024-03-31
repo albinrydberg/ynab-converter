303caac7ae632bf91880c8862b47a4eb0fe6b09c
Albin Rydberg <albin.rydberg@volvocars.com>
2024-03-31 15:59:49 +0200

    Polish CLI

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use clap::{Args, Parser, Subcommand};
+use clap::{arg, Parser, ValueEnum};
 
 mod handelsbanken;
 mod nordea;
@@ -6,41 +6,53 @@ mod util;
 mod ynab;
 
 #[derive(Parser)]
-#[command(version, about, long_about = None)]
+#[command(version, about, long_about = None, arg_required_else_help = true)]
 struct Cli {
-    /// What bank format to use
-    #[command(subcommand)]
-    bank: Bank,
-}
-
-#[derive(Subcommand)]
-enum Bank {
-    Nordea(Arguments),
-    Handelsbanken(Arguments),
-}
-
-#[derive(Args)]
-struct Arguments {
-    #[arg(short, long)]
     input_file: String,
 
     #[arg(short, long, default_value_t = String::from("output.csv"))]
     output_file: String,
+
+    #[arg(value_enum, short, long, help = "Force read input as a specific bank")]
+    force: Option<Bank>,
+}
+
+#[derive(ValueEnum, Clone)]
+enum Bank {
+    Nordea,
+    Handelsbanken,
 }
 
 fn main() -> anyhow::Result<()> {
     let cli = Cli::parse();
 
-    match cli.bank {
-        Bank::Nordea(arguments) => convert_csv(nordea::Parser::new(), arguments),
-        Bank::Handelsbanken(arguments) => convert_csv(handelsbanken::Parser::new(), arguments),
+    match cli.input_file {
+        input_file if is_nordea(&input_file) => {
+            convert_csv(nordea::Parser::new(), input_file, cli.output_file)
+        }
+        input_file if is_handelsbanken(&input_file) => {
+            convert_csv(handelsbanken::Parser::new(), input_file, cli.output_file)
+        }
+        unrecognized => Err(anyhow::Error::msg(format!(
+            "Unrecognized file: {:?}",
+            unrecognized
+        ))),
     }
 }
 
-fn convert_csv(parser: impl ynab::Parser, arguments: Arguments) -> anyhow::Result<()> {
-    let input_file = arguments.input_file;
-    let output_file = arguments.output_file;
+fn is_handelsbanken(input_file: &str) -> bool {
+    input_file.ends_with(".xls")
+}
 
+fn is_nordea(input_file: &str) -> bool {
+    input_file.ends_with(".csv")
+}
+
+fn convert_csv(
+    parser: impl ynab::Parser,
+    input_file: String,
+    output_file: String,
+) -> anyhow::Result<()> {
     let ynab_rows = parser.parse(input_file)?;
     ynab::write_csv(output_file, ynab_rows)
 }
