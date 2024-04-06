494e0c99dd4847b7bcf33a13f888c590feee1521
Albin Rydberg <albin.rydberg@volvocars.com>
2024-04-06 15:26:33 +0200

    Fix nordea bugs

--- src/nordea/transaction.rs
+++ src/nordea/transaction.rs
@@ -1,8 +1,8 @@
 use chrono::NaiveDate;
 
-#[derive(serde::Deserialize, Debug)]
+#[derive(serde::Deserialize, Debug, PartialEq)]
 pub struct Transaction {
-    #[serde(rename = "Bokföringsdag")]
+    #[serde(rename = "Bokföringsdag", with = "nordea_date")]
     pub timestamp: NaiveDate,
     #[serde(rename = "Belopp", with = "swedish_float")]
     pub amount: f32,
@@ -32,5 +32,29 @@ mod swedish_float {
     {
         let float_string = String::deserialize(deserializer)?;
         util::parse_swedish_float(float_string).map_err(Error::custom)
+    }
+}
+
+mod nordea_date {
+    use chrono::NaiveDate;
+    use serde::{Deserialize, Deserializer};
+    use serde::de::Error;
+
+    const DATE_FMT: &str = "%Y-%m-%d";
+    const DATE_FMT_2: &str = "%Y/%m/%d";
+    const RESERVED_KEYWORD: &str = "Reserverat";
+
+    pub fn deserialize<'de, D>(deserializer: D) -> Result<NaiveDate, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let date_string = String::deserialize(deserializer)?;
+        
+        if date_string.eq(RESERVED_KEYWORD) {
+            return Err(Error::custom("Ledger date was 'reserved'"));
+        }
+        
+        NaiveDate::parse_from_str(&date_string, DATE_FMT)
+            .or_else(|_| NaiveDate::parse_from_str(&date_string, DATE_FMT_2).map_err(Error::custom))
     }
 }
