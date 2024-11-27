abdcd9368158e34a866295727d83bfd6420b9663
Albin Rydberg <albin.rydberg@volvocars.com>
2024-11-27 10:58:25 +0100

    Move convertible trait

--- src/ynab/convertible.rs
+++
@@ -1,5 +0,0 @@
-use crate::ynab::Transaction;
-
-pub trait Convertible {
-    fn to_ynab(self) -> Transaction;
-}
--- src/ynab/mod.rs
+++ src/ynab/mod.rs
@@ -1,7 +1,9 @@
-pub use convertible::Convertible;
 pub use csv::YnabWriter;
 pub use transaction::Transaction;
 
-mod convertible;
 mod csv;
 mod transaction;
+
+pub trait Convertible {
+    fn to_ynab(self) -> Transaction;
+}
